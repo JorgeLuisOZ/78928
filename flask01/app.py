c7e7ab57c4daf9b9bb7e374ee3b960c2ad5f96c7
Jorge Luis Ortega Zenteno <zs22016127@estudiantes.uv.mx>
2025-03-07 10:39:05 -0600

    rutas en flask01

--- flask01/app.py
+++ flask01/app.py
@@ -2,10 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-
+@app.route('/saludar')
 def hola_mundo():
-    return 'Hola Mundo!'
+    return 'Hola mundo!'
+
+@app.route('/adios')
+def adios_mundo():
+    return 'Adios mundo!'
+
+@app.route('/hola')
+def hola_html():
+    return '<h1 style="color:blue">Hola HTML</h1>'
+
+@app.route('/json')
+def algo():
+    return '{"nombre":"john"}'
+
+@app.route('/xml')
+def xml():
+    return '<?xml version="1.0"?><nombre>john</nombre>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
